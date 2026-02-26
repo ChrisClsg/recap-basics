45dd58261c68cd3a3f0e77a2d090d2bced55bf8c
ChrisClsg <chris@clsg.de>
2026-02-26 14:23:00 +0100

    Introduce hasMinLength fn

--- src/main/java/de/clsg/App.java
+++ src/main/java/de/clsg/App.java
@@ -5,5 +5,10 @@ public class App
     public static void main( String[] args ) {
     }
 
+    static boolean hasMinLength(String pwd) {
+        int minLength = 8;
+
+        return pwd != null && !pwd.isBlank() && pwd.length() >= minLength;
+    }
 
 }
--- src/test/java/de/clsg/AppTest.java
+++ src/test/java/de/clsg/AppTest.java
@@ -3,8 +3,22 @@ package de.clsg;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class AppTest
 {
+    @ParameterizedTest
+    @CsvSource({
+        "null, false",
+        "         , false",
+        "abc, false",
+        "abcdefg, false",
+        "abcdefgh, true",
+        "abcdefghabcdefghabcdefghabcdefgh, true",
+    })
+    public void hasMinLength_returnsOnlyTrue_whenLengthEqualOrGrater8(String str, boolean exp) {
+        assertEquals(exp, App.hasMinLength(str));
+    }
 
 }
