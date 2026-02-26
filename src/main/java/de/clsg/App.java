565432fdffdfa8876f9bfea573aa2ea3687dec3d
ChrisClsg <chris@clsg.de>
2026-02-26 15:03:25 +0100

    Fix indentation mismatch

--- src/main/java/de/clsg/App.java
+++ src/main/java/de/clsg/App.java
@@ -2,42 +2,42 @@ package de.clsg;
 
 public class App
 {
-    public static void main( String[] args ) {
+  public static void main( String[] args ) {
+  }
+
+  static boolean hasMinLength(String pwd) {
+    int minLength = 8;
+
+    return pwd != null && !pwd.isBlank() && pwd.length() >= minLength;
+  }
+
+  static boolean containsDigit(String pwd) {
+    char[] chars = pwd.toCharArray();
+
+    for(int i = 0; i < chars.length; i++) {
+      if(Character.isDigit(chars[i])) return true;
     }
 
-    static boolean hasMinLength(String pwd) {
-        int minLength = 8;
+    return false;
+  }
 
-        return pwd != null && !pwd.isBlank() && pwd.length() >= minLength;
+  static boolean containsUpperChar(String pwd) {
+    char[] chars = pwd.toCharArray();
+
+    for(int i = 0; i < chars.length; i++) {
+      if(Character.isUpperCase(chars[i])) return true;
     }
 
-    static boolean containsDigit(String pwd) {
-        char[] chars = pwd.toCharArray();
+    return false;
+  }
 
-        for(int i = 0; i < chars.length; i++) {
-            if(Character.isDigit(chars[i])) return true;
-        }
+  static boolean containsLowerChar(String pwd) {
+    char[] chars = pwd.toCharArray();
 
-        return false;
+    for(int i = 0; i < chars.length; i++) {
+      if(Character.isLowerCase(chars[i])) return true;
     }
 
-    static boolean containsUpperChar(String pwd) {
-        char[] chars = pwd.toCharArray();
-
-        for(int i = 0; i < chars.length; i++) {
-            if(Character.isUpperCase(chars[i])) return true;
-        }
-
-        return false;
-    }
-
-    static boolean containsLowerChar(String pwd) {
-        char[] chars = pwd.toCharArray();
-
-        for(int i = 0; i < chars.length; i++) {
-            if(Character.isLowerCase(chars[i])) return true;
-        }
-
-        return false;
-    }
+    return false;
+  }
 }
--- src/test/java/de/clsg/AppTest.java
+++ src/test/java/de/clsg/AppTest.java
@@ -8,46 +8,46 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class AppTest
 {
-    @ParameterizedTest
-    @CsvSource({
-        "null, false",
-        "         , false",
-        "abc, false",
-        "abcdefg, false",
-        "abcdefgh, true",
-        "abcdefghabcdefghabcdefghabcdefgh, true",
-    })
-    public void hasMinLength_returnsOnlyTrue_whenLengthEqualOrGrater8(String str, boolean exp) {
-        assertEquals(exp, App.hasMinLength(str));
-    }
+  @ParameterizedTest
+  @CsvSource({
+    "null, false",
+    "         , false",
+    "abc, false",
+    "abcdefg, false",
+    "abcdefgh, true",
+    "abcdefghabcdefghabcdefghabcdefgh, true",
+  })
+  public void hasMinLength_returnsOnlyTrue_whenLengthEqualOrGrater8(String str, boolean exp) {
+    assertEquals(exp, App.hasMinLength(str));
+  }
 
-    @Test
-    public void containsDigit_returnsTrue_whenStringContainsDigit() {
-       assertTrue(App.containsDigit("abc1"));
-    }
+  @Test
+  public void containsDigit_returnsTrue_whenStringContainsDigit() {
+  assertTrue(App.containsDigit("abc1"));
+  }
 
-    @Test
-    public void containsDigit_returnsFalse_whenStringContainsNoDigit() {
-       assertFalse(App.containsDigit("abc"));
-    }
+  @Test
+  public void containsDigit_returnsFalse_whenStringContainsNoDigit() {
+    assertFalse(App.containsDigit("abc"));
+  }
 
-    @Test
-    public void containsUpperChar_returnsTrue_whenStringContainsUpperChar() {
-       assertTrue(App.containsUpperChar("aBc"));
-    }
+  @Test
+  public void containsUpperChar_returnsTrue_whenStringContainsUpperChar() {
+    assertTrue(App.containsUpperChar("aBc"));
+  }
 
-    @Test
-    public void containsUpperChar_returnsFalse_whenStringContainsNoUpperChar() {
-       assertFalse(App.containsUpperChar("abc"));
-    }
+  @Test
+  public void containsUpperChar_returnsFalse_whenStringContainsNoUpperChar() {
+    assertFalse(App.containsUpperChar("abc"));
+  }
 
-    @Test
-    public void containsLowerChar_returnsTrue_whenStringContainsLowerChar() {
-       assertTrue(App.containsLowerChar("ABc"));
-    }
+  @Test
+  public void containsLowerChar_returnsTrue_whenStringContainsLowerChar() {
+    assertTrue(App.containsLowerChar("ABc"));
+  }
 
-    @Test
-    public void containsLowerChar_returnsFalse_whenStringContainsNoLowerChar() {
-       assertFalse(App.containsLowerChar("ABC"));
-    }
+  @Test
+  public void containsLowerChar_returnsFalse_whenStringContainsNoLowerChar() {
+    assertFalse(App.containsLowerChar("ABC"));
+  }
 }
